35d9cac536d9cee4632d34da9da088d2b78a461f
Ali-Haider-Bukhari <ali.haider@doctornow.io>
2024-03-16 07:28:44 +0500

    updated

--- ERP/screens/Attandance/Attandance.js
+++ ERP/screens/Attandance/Attandance.js
@@ -1,7 +1,62 @@
-import { useState } from 'react';
+import { useState,useContext,useEffect } from 'react';
+import { useNavigation } from '@react-navigation/native';
 import { View,ScrollView, Text,ProgressBarAndroid, Button ,StyleSheet ,SafeAreaView, Image} from 'react-native';
+import { AuthContext } from '../../contexts/AuthContext';
+import { useGlobalContext } from '../../contexts/GlobalContext';
+import { Python_Url, getToken, removeToken } from '../../utils/constants';
 
 export default function AttandanceScreen() {
+const navigation = useNavigation();
+const {user} = useContext(AuthContext)
+const {courses,setCourses} = useGlobalContext()
+
+    async function getCourses(token){
+        
+    try {
+        const url = `${Python_Url}/courses/${user._id.$oid}`
+        const response = await fetch(url, {
+          method: 'GET',
+          headers: { 
+            'Content-Type': 'application/json',
+            Authorization: token,
+          }
+        });
+    
+        const data = await response.json();
+    
+        if (response.ok) {
+          // Handle successful response
+          console.log('Response data:', data[0].teacher_id);
+          setCourses(data)
+        } else {
+          // Handle error response
+          if(data.message == "Token has expired"){
+            AlertComponent({
+                title:'Message',
+                message:'Session Expired!!',
+                turnOnOkay:false,
+                onOkay:()=>{},
+                onCancel:()=>{
+                  ToastAndroid.show("Please Login to Continue",ToastAndroid.SHORT);
+                  removeToken()
+                  navigation.navigate("Login")
+                }},)
+          }
+          console.error('Error:', data);
+        }
+      } catch (error) {
+        // Handle network or other errors
+        console.error('Error:', error);
+      }
+}
+useEffect(() => {
+  if(user.role == "STUDENT"){
+    getToken()
+        .then((token) => {getCourses(token)})
+    
+  }
+}, [user])
+
   const [subjects , setSubjects] = useState([
     {
       subjectName:"Technopreneurship",
@@ -58,18 +113,18 @@ export default function AttandanceScreen() {
       
       <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
       
-      {subjects.map((data,index)=>(
+      {courses.map((data,index)=>(
         <View key={index} style={{marginTop:40,display:'flex',alignSelf:'center',width:'88%',height:130,border:1,borderWidth:1,borderColor:'rgba(226,226,226,255)'}}>
 
             <View style={{height:'50%',backgroundColor:'rgb(0, 174, 255)'}}>
               <View style={{backgroundColor:'rgba(222,149,0,255)',alignSelf:'flex-end',padding:2,width:80,borderRadius:2,height:21,display:'flex',flexDirection:'row',justifyContent:'center'}}>
-                <Text style={{color:'white',fontWeight:'bold',fontSize:12}}>{data.season}</Text>
+                <Text style={{color:'white',fontWeight:'bold',fontSize:12}}>{"SPRING 2024"}</Text>
               </View>
-            <Text style={{color:'white',fontWeight:'bold',marginLeft:20,fontSize:16,position:'relative',bottom:4}}>{data.subjectName}</Text>
-            <Text style={{color:'white',fontWeight:'bold',marginLeft:20,fontSize:11,position:'relative',bottom:4}}>{"Credit Hrs: "+data.creditHrs}</Text>
+              <Text style={{color:'white',fontWeight:'bold',marginLeft:20,fontSize:16,position:'relative',bottom:4}}>{data.course_name}</Text>
+            <Text style={{color:'white',fontWeight:'bold',marginLeft:20,fontSize:11,position:'relative',bottom:4}}>{"Credit Hrs: "+data.credit_hour}</Text>
             </View>
             <View style={{height:'50%',display:'flex',justifyContent:'center',alignItems:'center'}}>
-            <View style={{display:'flex',flexDirection:'row',justifyContent:'flex-start',width:'85%'}}><Text>{"Attandance: "+data.attandancePercentage+"%"}</Text></View>
+            <View style={{display:'flex',flexDirection:'row',justifyContent:'flex-start',width:'85%'}}><Text>{"Attandance: "+"92"+"%"}</Text></View>
             <ProgressBarAndroid
               styleAttr="Horizontal"
               indeterminate={false}
